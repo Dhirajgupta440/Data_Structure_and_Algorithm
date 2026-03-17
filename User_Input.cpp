98ecb013fe924aa9f7a5933078927eef074ca8f8
Dhirajgupta440 <157223950+Dhirajgupta440@users.noreply.github.com>
2026-03-17 10:29:43 +0530

    SF

---
+++ User_Input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int apples;
+    cout << "Enter the number of apples:" << endl;
+    cin >> apples;
+    cout << "The number of apples is: " << apples << endl;
+    return 0;
+}
